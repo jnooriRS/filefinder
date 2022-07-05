becee6229929797d95165084f8edc158b0c8ecb1
jnooriRS <jonas.noori@riversafe.co.uk>
2022-07-05 12:09:19 +0100

    sudocode

--- mr-file-finder.sh
+++ mr-file-finder.sh
@@ -1,3 +1,15 @@
 #--version=prints out version of the application
 #--help= prints out application help
 #scan-folder=Dir=folder to scan for file
+#Scan all files with type
+#create new folder
+#append all files into new folder
+
+filefinder(){
+    echo what folder would you like to scan?
+    echo $Directory
+#loop through folder and count/increment different types of file- using regex
+#place each file into seperate folder
+echo would you like to see your folder? 
+#conditional if yes print ls if no exit application
+}
