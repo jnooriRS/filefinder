6a2edc7f3e38405c320d5865dda978b9c246a89e
jnooriRS <jonas.noori@riversafe.co.uk>
2022-07-05 16:05:34 +0100

    find commands commented

--- mr-file-finder.sh
+++ mr-file-finder.sh
@@ -16,4 +16,9 @@ echo would you like to see your folder?
 #conditional if yes print ls if no exit application
 }
 
-ls -lx
+#ls -lx by extention
+#-d directory exists
+#find . -inname "*.md" | sort -r find all markdownfiles in working directory
+#find . inname "*$go_to_directory" | sort -r --output (save list to file)
+#find "$go_to_directory" -type d
+#find -name "XXX" -and -type d
